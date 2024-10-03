37a296ae921aa5a7385b42eaf66ed0fd9cac00d9
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2024-10-03 18:40:40 +0300

    first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC, useState } from 'react';
 import './App.css';
 
-function App() {
+type BtnProps = {text: string, onClick: ()=>void};
+type AppProps = {title: string};
+
+const AppBtn: FC<BtnProps> = ({text, onClick}) => {return (<button className='btn' type='button' onClick={onClick}>{text}</button>)}
+
+const App: FC<AppProps> = ({title}) => {
+  const [counter, setCounter] = useState(0)
+  const clickIncrement = ()=> setCounter(counter+1);
+  const clickDecrement = ()=>( counter === 0? 0: setCounter(counter-1));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{title}: {counter} times</h1>
+      <AppBtn text='Click+' onClick={clickIncrement}/>
+      <AppBtn text='Click-' onClick={clickDecrement}/>
+
     </div>
   );
 }
